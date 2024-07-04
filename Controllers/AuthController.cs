a6c21996f62c50063830de13d417f70c4f665702
haboullail <haboullail@hotmail.com>
2024-07-05 01:47:29 +0200

    First Commit BackEnd

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,113 @@
+﻿using BackEnd.Api.Data;
+using BackEnd.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BackEnd.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _config;
+
+        // Constructor
+        public AuthController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        /// <summary>
+        /// Register
+        /// </summary>
+        /// <param name="registerDto"></param>
+        /// <returns></returns>
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = new ApplicationUser { UserName = registerDto.UserName, Email = registerDto.Email };
+
+                var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+                if (result.Succeeded)
+                {
+                    return Ok(new { Result = result, StatusCode = 200, StatusLebel = "Succes" });
+                }
+
+                return BadRequest(result.Errors);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid) { return BadRequest(ModelState); }
+
+                var result = await _signInManager.PasswordSignInAsync(loginDto.UserName, loginDto.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    var appUser = await _userManager.FindByNameAsync(loginDto.UserName);
+                    var token = GenerateJwtToken(appUser);
+                    return Ok(new { Token = token, StatusCode = 200, StatusLabel = "Succes" });
+                }
+
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+
+        }
+
+        public string GenerateJwtToken(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        // Const 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        
+    }
+    public class ApplicationUser : IdentityUser { }
+}
